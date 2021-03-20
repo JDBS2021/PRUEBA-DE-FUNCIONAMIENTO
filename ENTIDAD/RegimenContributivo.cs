bbe5d67f9a562c350a3e3c90546253dd9b709860
Jesús Beleño <persomultimate@gmail.com>
2021-03-20 17:00:28 -0500

    XD

--- ENTIDAD/RegimenContributivo.cs
+++ ENTIDAD/RegimenContributivo.cs
@@ -6,48 +6,50 @@ namespace ENTIDAD
 {
     public class RegimenContributivo : LiquidacionCM
     {
-        public RegimenContributivo()
-
+        public RegimenContributivo() 
         {
-
+           
+                  
         }
 
-        public RegimenContributivo(long numeroLiquidacion, long pacienteID, string tipoAfiliacion, long salarioPaciente)
-        base(numeroLiquidacion, pacienteID, tipoAfiliacion, salarioPaciente)
+        public RegimenContributivo(long numeroLiquidacion, long pacienteID, string tipoAfiliacion, long salarioPaciente, int salarioServicio)
+            : base(numeroLiquidacion, pacienteID, tipoAfiliacion, salarioPaciente, salarioServicio) { }
 
+        public override void CalcularTarifa()
         {
-
+            if (SalarioDevengado < 2)
+            {
+                tasa = 15;
+            }
+            else if (SalarioDevengado >= 2 && SalarioDevengado <= 5)
+            {
+                tasa = 20;
+            }
+            else if (SalarioDevengado > 5)
+            {
+                tasa = 25;
+            }
         }
 
-    public override void CalcularTarifa()
-    {
-        if (SalarioPaciente < 2)
+        public override void CalcularTope()
         {
-            tasa = 0.15;
+            if (SalarioDevengado < 2)
+            {
+                Tope = 250000;
+            }
+            else if (SalarioDevengado >= 2 && SalarioDevengado <= 5)
+            {
+                Tope = 9000000;
+            }
+            else if (SalarioDevengado > 5)
+            {
+                Tope = 1500000;
+            }
         }
-        else if (SalarioPaciente >= 2 && SalarioPaciente <= 5)
-        {
-            tasa = 0.20;
-        }
-        else if (SalarioPaciente > 5)
-        {
-            tasa = 0.25;
-        }
+
+
     }
 
-    public override void CalcularTope()
-    {
-        if (SalarioPaciente < 2)
-        {
-            Tope = 250000;
-        }
-        else if (SalarioPaciente >= 2 && SalarioPaciente <= 5)
-        {
-            Tope = 9000000;
-        }
-        else if (SalarioPaciente > 5)
-        {
-            Tope = 1500000;
-        }
-    }
+
+
 }
